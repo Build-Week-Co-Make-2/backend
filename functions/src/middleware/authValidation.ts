c02a56299aab49a8cecb2b1695b0f7e2ec1a2dd1
victor tran <victor.tran@tempestas.io>
2020-06-17 13:19:32 -0400

    write login endpoint with validation

--- functions/src/middleware/authValidation.ts
+++ functions/src/middleware/authValidation.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from 'express';
 import { getUserWithEmail } from '../database/users';
+import bcrypt from 'bcryptjs';
+
 export const validateRegisterEmail: RequestHandler = async (
     req,
     _res,
@@ -19,6 +21,35 @@ export const validateRegisterEmail: RequestHandler = async (
             });
         }
         next();
+    } catch (e) {
+        next({ status: 500, message: 'Unexpected server response' });
+    }
+};
+
+export const validateUser: RequestHandler = async (req, _res, next) => {
+    const { email } = req.body;
+    try {
+        const users = await getUserWithEmail(email);
+        const user = users.docs
+            .find((doc) => doc.data().email === email)
+            ?.data();
+        if (user) {
+            // validate password here and now
+            const valid = bcrypt.compareSync(req.body.password, user.hash);
+            if (valid) {
+                req.body.user = user;
+                next();
+                return;
+            }
+            next({
+                status: 400,
+                message: 'Email or Password incorrect',
+            });
+        }
+        next({
+            status: 400,
+            message: 'Email or Password incorrect',
+        });
     } catch (e) {
         next({ status: 500, message: 'Unexpected server response' });
     }
