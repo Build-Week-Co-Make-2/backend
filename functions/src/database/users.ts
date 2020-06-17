4be4de9a24de4a5d55dc3a49b45b16110931ffe7
victor tran <victor.tran@tempestas.io>
2020-06-17 13:17:11 -0400

    write add users helper function

--- functions/src/database/users.ts
+++ functions/src/database/users.ts
@@ -1,5 +1,17 @@
 import database from './setUp';
 
+type AddUserType = {
+    email: string;
+    hash: string;
+    zip: string;
+};
+
+type AddUserCallback = (
+    user: AddUserType,
+) => Promise<
+    FirebaseFirestore.DocumentReference<FirebaseFirestore.DocumentData>
+>;
+
 const Users = database.collection('users');
 
 export const getUserWithEmail: (
@@ -8,6 +20,11 @@ export const getUserWithEmail: (
     FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData>
 > = async (email: string) => {
     return await Users.where('email', '==', email).get();
+};
+
+export const addUser: AddUserCallback = async (user: AddUserType) => {
+    const docRef = await Users.add(user);
+    return docRef;
 };
 
 export default Users;
