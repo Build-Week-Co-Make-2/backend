b7f8a94dada26b70157dba68b3185f55f8b74c8c
victor tran <victor.tran@tempestas.io>
2020-06-17 13:16:42 -0400

    create function to get all users with email

--- functions/src/database/users.ts
+++ functions/src/database/users.ts
@@ -1,3 +1,11 @@
 import database from './setUp';
 
 const Users = database.collection('users');
+
+export const getUserWithEmail: (
+    email: string,
+) => Promise<
+    FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData>
+> = async (email: string) => {
+    return await Users.where('email', '==', email).get();
+};
