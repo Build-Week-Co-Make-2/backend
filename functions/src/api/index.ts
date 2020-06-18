2fa4cfca8340a490ea03531fe4122b60c44b52ef
victor tran <victor.tran@tempestas.io>
2020-06-18 13:46:12 -0400

    import post router to be used

--- functions/src/api/index.ts
+++ functions/src/api/index.ts
@@ -1,12 +1,15 @@
 import express, { ErrorRequestHandler } from 'express';
 import cors from 'cors';
 import authRouter from '../router/auth';
+import postRouter from '../router/posts';
 
 const server = express();
 server.get('/', (req, res) => res.send('Hello World'));
 server.use(cors({ origin: true })); // this was the issue with reaching past root @ /
 
+server.use('/api/posts', postRouter);
 server.use('/api', authRouter);
+server.use('/api/posts', postRouter);
 
 type ErrorRequestType = {
     status: number;
