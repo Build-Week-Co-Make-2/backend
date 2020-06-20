e8761eac0f2a187d217277b956abeb88548ad97b
victor tran <victor.tran@tempestas.io>
2020-06-20 16:12:52 -0400

    update code to use new User class methods

--- functions/src/middleware/getAuthedUser.ts
+++ functions/src/middleware/getAuthedUser.ts
@@ -1,29 +1,18 @@
 import * as functions from 'firebase-functions';
 import { RequestHandler } from 'express';
 import jwt from 'jsonwebtoken';
-import Users from '../database/users';
+import { User } from '../database/users';
 
 export const getAuthedUser: RequestHandler = async (req, _res, next) => {
     const decoded: { email: string } = jwt.verify(
         req.headers.authorization as string,
         functions.config()['co-make'].jwt.secret,
     ) as { email: string };
-    // get user doc
-    Users.where('email', '==', decoded.email)
-        .get()
-        .then((docs) => {
-            // we have user now
-            const user = docs.docs.find(
-                (doc) => doc.data().email === decoded.email,
-            );
-
-            return user;
-        })
-        .then((user) => {
-            req.body.id = user?.id; // this is always going to be truthy regardless of typescript saying possible undefined bc of prev middlewares
-            next();
-        })
-        .catch((err) => {
-            next({ status: 500, message: 'Server error getting authed user' });
-        });
+    try {
+        const user = await User.getByEmail(decoded.email);
+        req.body.user = user;
+        next();
+    } catch (e) {
+        next({ status: 500, message: 'Server error getting authed user' });
+    }
 };
