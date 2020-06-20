e0d1663ddf0ea70e1b2981983dca6147e915e3eb
victor tran <victor.tran@tempestas.io>
2020-06-20 16:09:08 -0400

    add extra layer of error checking

--- functions/src/middleware/authenticate.ts
+++ functions/src/middleware/authenticate.ts
@@ -3,21 +3,31 @@ import { RequestHandler } from 'express';
 import jwt from 'jsonwebtoken';
 
 export const authenticate: RequestHandler = async (req, _res, next) => {
-    if (
-        req.headers.authorization &&
-        jwt.verify(
-            req.headers.authorization,
-            functions.config()['co-make'].jwt.secret,
-        )
-    ) {
-        // if we get here then there is something in the authorization header
-        // and a token was present and verified
-
-        next();
+    if (!req.headers.authorization) {
+        next({
+            status: 403,
+            message: 'Operation requires authenticated use. Please sign in',
+        });
         return;
     }
-    next({
-        status: 403,
-        message: 'Operation requires authenticated use. Please sign in',
-    });
+    try {
+        const decoded = jwt.verify(
+            req.headers.authorization,
+            functions.config()['co-make'].jwt.secret,
+        );
+        if (decoded) {
+            // if we get here then there is something in the authorization header
+            // and a token was present and verified
+
+            next();
+            return;
+        }
+        next({
+            status: 403,
+            message: 'Operation requires authenticated use. Please sign in',
+        });
+    } catch (e) {
+        console.log(e);
+        next({ status: 403, message: 'Token has issues, please sign in.' });
+    }
 };
