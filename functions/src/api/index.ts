7e223b3f44c33853178ec9cd06f5a5dbc4b2b735
victor tran <victor.tran@tempestas.io>
2020-06-20 16:10:52 -0400

    add morgan and helmet to app

--- functions/src/api/index.ts
+++ functions/src/api/index.ts
@@ -1,5 +1,8 @@
-import express, { ErrorRequestHandler } from 'express';
+import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+
 import authRouter from '../router/auth';
 import postRouter from '../router/posts';
 import errorHandler from '../middleware/errorHandler';
@@ -7,6 +10,8 @@ import errorHandler from '../middleware/errorHandler';
 const server = express();
 server.get('/', (req, res) => res.send('Hello World'));
 server.use(cors({ origin: true })); // this was the issue with reaching past root @ /
+server.use(helmet());
+server.use(morgan('tiny'));
 
 server.use('/api/posts', postRouter);
 server.use('/api', authRouter);
