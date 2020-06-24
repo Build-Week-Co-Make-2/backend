e51b4c83e15070ee88a23cfc97203e79845dd07d
victor tran <victor.tran@tempestas.io>
2020-06-24 16:56:36 -0400

    add onDelete trigger for cascade deletes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,18 @@
 import * as functions from 'firebase-functions';
 import server from './api';
+import Posts from './database/posts';
 
 export const app = functions.https.onRequest(server);
+export const onDeleteUser = functions.firestore
+    .document('/users/{id}')
+    .onDelete(async (snapshot, context) => {
+        // grab id out of context
+        const id = context.params?.id;
+
+        if (!id) return; // just fyi, id will always be here but for type checking purposes this will be put here
+
+        // now grab all posts where owner === id
+        (await Posts.where('owner', '==', id).get()).forEach(async (doc) => {
+            await doc.ref.delete();
+        });
+    });
